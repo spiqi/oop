9a3cf7b08360e54f89676a97fefc6ced5b00a98c
Haoran <a@h88i.com>
2020-10-13 20:34:23 +0800

    Update exp 2 _1.cpp

--- exp 2 _1.cpp
+++ exp 2 _1.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
 #include<math.h>
+
 int perform() {
 
 	printf("Solving the root of equation ax^2+bx+c=0\n Input a b c\n");
@@ -8,7 +9,7 @@ int perform() {
 	scanf("%lf %lf %lf", &a, &b, &c);
 	if (a == 0) printf("x= %lf ", -c / b);
 	else {
-		int delta = (b * b) - (4 * a*c);
+		double delta = (b * b) - (4 * a*c);
 		if (delta > 0) {
 
 			delta = sqrt(delta);
@@ -27,7 +28,13 @@ int perform() {
 
 int swap() {
 	int a, b;
-
+	return 0;
+}
+int years() {
+	int y, m, d;// 年月日
+	scanf("%d%d%d", &y, &m, &d);
+	return 0;
+	int mo[13] = { 0,0,31,28,31,30,31,30,31,31,30,31,30, };//从一到十二月 定义
 }
 
 int main() {
