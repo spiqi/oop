aa7fed6f771d624f59c0c4f338119cdc0b71826f
Haoran <a@h88i.com>
2020-10-27 20:05:56 +0800

    Update exp4.cpp

--- exp4/exp4.cpp
+++ exp4/exp4.cpp
@@ -1,13 +1,23 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include<iostream>
-
 int fib() {
-
+	int n;
+	printf("input the length of Fibonacii :n:\n");
+	scanf("%d", &n);
+	int *p = new int[n+1];
+	*(p + 1) = 1;
+	*(p + 2) = 1;
+	for (int i = 3; i <= n; i++)
+		*(p + i) = *(p + i - 1) + *(p + i - 2);
+	for (int i = 1; i <= n; i++)
+		printf("%d", *(p + i));
+	return 0;
 }
 int fx() {
-
+	return 0;
 }
 int game() {
-
+	return 0;
 }
 
 
