e923be64e88748467d5c77544669ae39230028b9
Haoran <a@h88i.com>
2020-12-08 17:19:59 +0800

    Create task1.cpp
    【任务1】设计一个基类base，其内含有数据成员（public：int a， protected： int b，private： int c，public：static int count）和函数成员（输出类的数据成员函数print（），统计类对象个数函数 static int statistic（）），然后请采用三种不同的继承方式由base类分别派生出三个子类：derived1，derived2，derived3，请根据上述基类和派生类尝试编程论证下面的三个问题。（可参考课堂演示程序）
    
    派生类全盘接受基类的所有本类成员，其中包括基类的普通公有成员，保护成员和私有成员。
    
    根据继承类数据成员能否在类内或类外被访问的问题，探索分析三种不同继承方式各自的特点（参考课程ppt）。
    
    派生类对象被建立时派生类是如何调用构造函数的，给出构造函数调用的次序，析构函数析构次序，并分析其中规律。

---
+++ 5-1/task1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+class Base{
+    private:
+    int c;
+        
+        protected:
+    int b;
+    
+    public :
+    int a;
+static int count;
+Base(){
+    count ++;
+}
+    int print(){
+        //output
+    }
+
+    static int statistic(){
+
+    }
+}
+int main(){
+    return 0;
+}
